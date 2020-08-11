070b954b6a6bf2614c5349433ba3acbb3768f768
gkimelblat <gabriekni@gmail.com>
2020-08-11 17:55:06 +0300

    Profile Picture Working

--- mysite_root/mysite/settings.py
+++ mysite_root/mysite/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'materialize_css_forms'
 
 LOGIN_REDIRECT_URL = 'blog-home'
