410fbc2639170ef57cb375244f221f5d1e915446
gkimelblat <gabrielkni@gmail.com>
2020-08-18 12:35:04 +0300

    local_settings included

---
+++ mysite/local_settings_example.py
@@ -0,0 +1,17 @@
+import os
+import django_heroku
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+AWS_ACCESS_KEY_ID=''
+AWS_SECRET_ACCESS_KEY=''
+AWS_STORAGE_BUCKET_NAME=''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
--- mysite/settings.py
+++ mysite/settings.py
@@ -140,17 +140,12 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587 # Less likely to be rejected: https://serversmtp.com/port-587/
 EMAIL_USE_TLS = True
 
-# original authentication data must be stored on .bash_profile.
-EMAIL_HOST_USER = os.environ.get('DB_USER') 
-EMAIL_HOST_PASSWORD = os.environ.get('DB_PASS')
 
 # Documentation: https://django-storages.readthedocs.io/en/latest/backends/amazon-S3.html
 #AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 #AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 #AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_ACCESS_KEY_ID='AKIAQDOB5Q2BN6GTNZCR'
-AWS_SECRET_ACCESS_KEY='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'
-AWS_STORAGE_BUCKET_NAME='gherman-first-django'
+
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
@@ -161,3 +156,9 @@ AWS_S3_REGION_NAME = 'us-east-2'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
+
+# Load local settings
+try:
+   from .local_settings import *
+except ImportError:
+    raise Exception('A local_settings.py file is required to run this project')
