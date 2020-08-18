cccb85de9d4eadb7a933a649a6480a6bdd712d9b
gkimelblat <gabrielkni@gmail.com>
2020-08-18 19:02:22 +0300

    heroku fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -148,10 +148,10 @@ AWS_S3_REGION_NAME = 'us-east-2'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
-
 # Load local settings
 try:
    from .local_settings import *
 except ImportError:
     pass
+
+django_heroku.settings(locals())
